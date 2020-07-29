10cd2a7b48f90519ac2b6b0103217df29e00434d
Tim <timofey.ilin.developer@gmail.com>
2020-07-29 14:13:18 +0300

    Созданы модели Article и ParseError, произведены миграции. Реализован скрипт habra_parser.py данный скрипт должен будет вызываться автоматически раз в сутки.

---
+++ api/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    url = models.URLField(verbose_name='Ссылка на статью', unique=True)
+    body_text = models.TextField(verbose_name='Текст статьи')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления добавления')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+
+class ParserError(models.Model):
+    error = models.CharField(max_length=200, verbose_name='Ошибка')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Время добавления добавления')
+
+    def __str__(self):
+        return self.error
+
+    class Meta:
+        verbose_name = 'Ошибка'
+        verbose_name_plural = 'Ошибки'
---
+++ habra_parser.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup as BS
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'habra_scrap.settings'
+application = get_wsgi_application()
+from api.models import Article, ParserError
+
+articles = []
+errors = []
+
+
+def parser():
+    """
+    Данная фунция парсит топ популярных статей с хабра за сутки.
+    Складывает все в LIST articles, в случае неправильного статус кода
+    ошибка кладется в LIST errors
+    :return: void
+    """
+    url = 'https://habr.com/ru/top/'
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BS(response.text, 'html.parser')
+        all_posts = soup.find_all('article', class_='post post_preview')
+        for post in all_posts:
+            title = post.find('h2', class_='post__title')
+            body_text = post.find('div', class_='post__text')
+            url = title.a['href']
+            articles.append({'title': title.text, 'url': url, 'body_text': str(body_text.text).strip()})
+    else:
+        errors.append({'error': f'Не верный статускод {response.status_code} возможно изменился url'})
+
+
+if __name__ == '__main__':
+    """
+    Получаем все статьи, если статьи получены, то удаляем старые и сохраняем новые
+    Поскольку время на реализацию ограничено, то сделано именно так.
+    Если есть ошибки то сохраняем их в базу.
+    """
+    parser()
+    if articles:
+        all_article = Article.objects.all()
+        all_article.delete()
+        for article_item in articles:
+            article = Article(**article_item)
+            article.save()
+
+    if errors:
+        for error_item in errors:
+            error = ParserError(**error_item)
+            error.save()
